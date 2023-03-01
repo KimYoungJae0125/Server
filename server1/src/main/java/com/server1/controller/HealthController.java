a97929158afe17f245c1a6d06ee53f4f310ce18c
kyj <uni0125@naver.com>
2023-03-01 15:39:09 +0900

    초기 그래들 멀티모듈 세팅

---
+++ server1/src/main/java/com/server1/controller/HealthController.java
@@ -0,0 +1,16 @@
+package com.server1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/health")
+@RestController
+public class HealthController {
+
+    @GetMapping
+    public String health() {
+        return "health";
+    }
+
+}
